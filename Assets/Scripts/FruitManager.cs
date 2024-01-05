e4018da438c50e2f1a1d98cc87704c5ea6b07136
Amanda Marino <124836202+AmandaOkapi@users.noreply.github.com>
2024-01-05 01:17:57 -0500

    ladies and gentlemen the weirdest collision checker ever. i hope it holds up....

--- Assets/Scripts/FruitManager.cs
+++ Assets/Scripts/FruitManager.cs
@@ -11,6 +11,17 @@ public class FruitManager : MonoBehaviour
 
     public Vector3 pos;
     public int myId;
+
+    public int backlog = 0;
+
+    public Collision sentCol;
+
+    public Quaternion r;
+
+    //list of tuples
+    public List<(int,Vector3, Quaternion)> backlogList  = new List<(int, Vector3, Quaternion)>();
+
+    //maybe i should set up a constantly resetting counter int that will panic if its bigger than 1
                 
     void Start()
     {
@@ -20,9 +31,59 @@ public class FruitManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        print(backlogList);
         if(makeFruit){
-            Instantiate(fruitArr[myId+1],pos, gameObject.transform.rotation);
-            makeFruit=false;
-        }
+            StartCoroutine(WaitForFiveSeconds());
+makeFruit=false;
     }
+
+    }
+        IEnumerator WaitForFiveSeconds()
+    {
+        // Waits for 5 seconds
+        yield return new WaitForSeconds(5f);
+
+        // This code executes after waiting for 5 seconds
+        Debug.Log("Waited for 5 seconds!");
+
+                    print("hi");
+            //sentCol.gameObject.myId =myId;
+            //get pos
+            //ContactPoint contact = sentCol.contacts[0];
+            //pos =contact.point;
+            List<(int,Vector3, Quaternion)> duplicates  = new List<(int, Vector3, Quaternion)>();
+            bool isDup=false;
+            int cnt=0;
+            foreach ((int,Vector3, Quaternion) item in backlogList){
+                cnt+=1;
+                        print(backlogList +" "+ cnt );
+
+                print(item);
+                int a = item.Item1; // Accessing the first item (int)
+                Vector3 position = item.Item2; // Accessing the second item (Vector3)
+                Quaternion rotation = item.Item3; // Accessing the third item (Quaternion)
+
+
+                //check for dups
+
+                    foreach ((int,Vector3, Quaternion) dup in duplicates){
+                        if ( (a==dup.Item1) && (position==dup.Item2 ) ){
+                            print("found");
+                            isDup=true;
+                            break;
+                        }
+                    }
+                duplicates.Add(item);
+
+                if (isDup==false){
+                    print("making" + item + "with" + a + position + rotation);
+                    Instantiate(fruitArr[a+1],position, rotation);
+                }
+
+                isDup=false;
+            }
+                //backlog-=2;
+            backlogList.Clear();
+        }
+    
 }
--- Assets/Scripts/Fruits.cs
+++ Assets/Scripts/Fruits.cs
@@ -52,22 +52,55 @@ void OnCollisionEnter(Collision collision)
             Debug.Log(gameObject.tag + "collision");
 
             if (myId+2< fruitManager.fruitArr.Length){
+                fruitManager.backlog+=1;
                 
+                ContactPoint contact = collision.contacts[0];
+                Vector3 pos = contact.point;
+
+                (int,Vector3, Quaternion) propertiesTuple = (myId, pos, transform.rotation);
+    print(propertiesTuple);
+                fruitManager.backlogList.Add(propertiesTuple);
+                fruitManager.makeFruit=true;
+
+                //StartCoroutine(spawnNewFruit(collision));
+
+              /*  
                 ContactPoint contact = collision.contacts[0];
                 
                 fruitManager.pos= contact.point;
                 fruitManager.myId =myId;
                 fruitManager.makeFruit=true;
                 //Instantiate(fruitManager.fruitArr[myId+1],pos, gameObject.transform.rotation);
-
+            */
+                Destroy(collision.gameObject);
+                Destroy(gameObject);
             }       
            
             //newFruit.myId=myId+1;
-            Destroy(collision.gameObject);
-            Destroy(gameObject);
+           
 
 
         }
 }
+/*
+    IEnumerator spawnNewFruit(Collision collision){
+        print("enumerator1");
 
+        while (fruitManager.backlog>=2){
+                fruitManager.sentCol=collision;
+                
+                ContactPoint contact = collision.contacts[0];
+                fruitManager.pos= contact.point;
+                fruitManager.myId =myId;
+                fruitManager.r =transform.rotation;
+
+
+                fruitManager.makeFruit=true;
+                print("enumerator2");
+            yield return null;
+        }
+        Destroy(collision.gameObject);
+        Destroy(gameObject);
+    }
+*/
 }
